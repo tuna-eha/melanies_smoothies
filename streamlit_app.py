db70a6b9373c59133db68cf3686ae417a0ccd425
tuna-eha <tansari9@gmail.com>
2026-01-15 14:43:54 -0500

    Restore Smoothiefroot API call for watermelon data
    Reintroduced the Smoothiefroot API call to fetch watermelon data and display it in the app.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,7 @@
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+
 
 # Write directly to the app
 st.title(':cup_with_straw: Customize Your Smoothie! :cup_with_straw:')
@@ -54,3 +52,7 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
+
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response.json())
